d5c6a3a350fae8c74d4b8bf7897bf4a8ef89c9e0
Shriram Ravi <shriramravi96@gmail.com>
2021-09-26 19:03:34 -0400

    File Reformat

--- Middle flask/app.py
+++ Middle flask/app.py
@@ -2,13 +2,15 @@ from flask import Flask, request, jsonify
 import requests
 import json
 
-app =  Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def check1():
     return 'Hello mic testing 1,2,3!'
 
-@app.route('/middle', methods = ['POST'])
+
+@app.route('/middle', methods=['POST'])
 def middle_check():
     username = request.form["UserName"]
     owner = request.form["Owner"]
@@ -18,5 +20,6 @@ def middle_check():
 
     return "<br>" + username + " requested access to " + repo + " repo from " + owner
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5001, debug = True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
