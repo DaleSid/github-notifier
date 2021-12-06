fe4a8c10d37d5e80a84c93c212fd0ac868e268f9
Shriram Ravi <shriramravi96@gmail.com>
2021-12-06 01:53:38 -0500

    Producer side code

--- api_call/api_check.py
+++ api_call/api_check.py
@@ -3,31 +3,33 @@ import os
 import json
 import pymongo
 from flask import Flask
+from kafka import KafkaProducer
 import time
 import random
+import json
 
 app = Flask(__name__)
 
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
 @app.route('/')
 def api_pull_to_db():
-    client = pymongo.MongoClient(host='backend_db',
-                        port=27017,
-                        username='root',
-                        password='pass',
-                        authSource="admin")
-    db = client.subscribers_db
-    topics_db = db.topics_db
+    producer = KafkaProducer(bootstrap_servers=['kafka-1:9092', 'kafka-2:9092', 'kafka-3:9092'], value_serializer=json_serializer)
+
+    topics_file = open('topics.json')
+    topics_data = json.load(topics_file)
 
     timeout = time.time() + 1200
 
     while(time.time() <= timeout):
-        cursor = topics_db.find({})
-        for document in cursor:
-            publisher = document['publisher']
+        for topic in topics_data['topics']:
+            publisher = topic['publisher']
             if(publisher == 'GitHub'):
-                owner = document['owner']
-                repo = document['repo']
-                last_update = document['last_update']
+                owner = topic['owner']
+                repo = topic['repo']
+                topic_name = publisher + "_" + owner + "_" + repo
+                last_update = topic['last_update']
                 query_url = f"https://api.github.com/repos/{owner}/{repo}/commits?since={last_update}"
                 
                 r = requests.get(query_url, headers={'Authorization': 'Bearer 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'})
@@ -36,30 +38,10 @@ def api_pull_to_db():
                 if 'documentation_url' in commit_messages:
                     return 'API Limit exceeded!'
 
-                # with open('checkfile.json') as f:
-                #     commit_messages = json.load(f)
-
-                db_push_dict =  {}
-                db_push_dict['message_type'] = 'publish'
-                db_push_dict['publisher'] = publisher
-                db_push_dict['owner'] = owner
-                db_push_dict['repo'] = repo
-                db_push_dict['commit_messages'] = commit_messages
-
-                if len(commit_messages):
-                    # db_push_json = json.dumps(db_push_dict)
-                    try:
-                        brokers = {'backend_broker_1':'5101', 'backend_broker_2':'5101', 'backend_broker_3':'5101'}
-                        # brokers = {'backend_broker_1':'5101'}
-                        broker_add = random.choice(list(brokers.keys()))
-                        response = requests.post(f'http://{broker_add}:{brokers[broker_add]}/commits_notifier', data = json.dumps(db_push_dict))
-                    except requests.exceptions.RequestException as e:
-                        return str(e)
-                        # return 'Cannot reach server!'
-                    # return str(db_push_dict)
-                    # return response.text
+                for message in commit_messages:
+                    producer.send(topic_name, message)
+                    time.sleep(2)
         time.sleep(60)
-    client.close()
     return 'This is done!'
 
 if __name__ == "__main__":
