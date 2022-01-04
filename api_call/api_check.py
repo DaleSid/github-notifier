23c4b5e3f5751b55e02003e8ad0a220ed5cb884a
DaleSid <91238151+DaleSid@users.noreply.github.com>
2022-01-04 15:04:40 -0500

    Removed github personal access token

--- api_call/api_check.py
+++ api_call/api_check.py
@@ -30,7 +30,7 @@ def api_pull_to_db():
                 last_update = document['last_update']
                 query_url = f"https://api.github.com/repos/{owner}/{repo}/commits?since={last_update}"
                 
-                r = requests.get(query_url, headers={'Authorization': 'Bearer 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'})
+                r = requests.get(query_url, headers={'Authorization': 'Bearer `insert github access token`'})
                 commit_messages = r.json()
 
                 if 'documentation_url' in commit_messages:
